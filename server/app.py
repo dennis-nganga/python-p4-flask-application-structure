d3b804cbe75a9297df77450d596fd20e405f353e
dennis-nganga <denniskimani416@gmail.com>
2023-06-19 23:06:45 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-
 app = Flask(__name__)
+@app.route('/')
+@app.route('/home')
+def index():
+    return  f'<h1> this is the first line of Flask</h1>'
+
+@app.route('/<username>')
+def user(Username):
+    return f'<h1>heres my username{Username}</h1>'
+
+app.run(debug=True)
+
